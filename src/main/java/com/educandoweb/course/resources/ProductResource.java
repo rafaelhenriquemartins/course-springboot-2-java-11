c2d57d558fd48fb69c900b8eb6f30fa9aa422f3b
Rafael Martins <rafaelrhm.27@gmail.com>
2020-09-20 15:43:34 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/resources/ProductResource.java
+++ src/main/java/com/educandoweb/course/resources/ProductResource.java
@@ -11,30 +11,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.services.ProductService;
+
 //pra dizer quew essa calasse é implementada por um controlador rest, precisa desse controlador
 @RestController
 @RequestMapping(value = "/products") // nome do recurso, com o caminho do meu recurso
 public class ProductResource {
-	
-	//criando o metodo que sera o endpoint para acessar os usuarios, user é o nome da minha classe
-	//indicando qu esse metodo é um metoddo que responde a reequisiçaõ do tipo get do http
-	
+
+	// criando o metodo que sera o endpoint para acessar os usuarios, user é o nome
+	// da minha classe
+	// indicando qu esse metodo é um metoddo que responde a reequisiçaõ do tipo get
+	// do http
+
 	@Autowired
 	private ProductService service;
-	
+
 	@GetMapping
-	public ResponseEntity <List<Product>> findAll(){
+	public ResponseEntity<List<Product>> findAll() {
 		List<Product> list = service.findAll();
-		return ResponseEntity.ok().body(list); // retorna ok no http, e retorna o corpo da resposta	
+		return ResponseEntity.ok().body(list); // retorna ok no http, e retorna o corpo da resposta
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Product> findById(@PathVariable Long id){
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
 		Product obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	
-	
-	
+
 }
