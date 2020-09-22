620e2d34d57166abc4e2619b14fef34d7fd97b13
Rafael Martins <rafaelrhm.27@gmail.com>
2020-09-21 22:17:33 -0300

    User insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,40 +1,52 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
+
+
 //pra dizer quew essa calasse é implementada por um controlador rest, precisa desse controlador
 @RestController
 @RequestMapping(value = "/users") // nome do recurso, com o caminho do meu recurso
 public class UserResource {
-	
-	//criando o metodo que sera o endpoint para acessar os usuarios, user é o nome da minha classe
-	//indicando qu esse metodo é um metoddo que responde a reequisiçaõ do tipo get do http
-	
+
+	// criando o metodo que sera o endpoint para acessar os usuarios, user é o nome
+	// da minha classe
+	// indicando qu esse metodo é um metoddo que responde a reequisiçaõ do tipo get
+	// do http
+
 	@Autowired
 	private UserService service;
-	
+
 	@GetMapping
-	public ResponseEntity <List<User>> findAll(){
+	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list); // retorna ok no http, e retorna o corpo da resposta	
+		return ResponseEntity.ok().body(list); // retorna ok no http, e retorna o corpo da resposta
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
+	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	
-	
-	
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
 }
